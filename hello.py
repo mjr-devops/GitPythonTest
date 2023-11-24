f08916956f1fa9d3983afe820ea93b5bc8a7128d
mjr-devops <mjr.devops@gmail.com>
2023-11-24 13:07:33 -0500

    First commit for testing python code in git

---
+++ hello.py
@@ -0,0 +1,13 @@
+import os
+os.system('clear')
+
+first_name = 'Michael'
+age = '41'
+fav_pizza = {
+    "Michael": "Pepperoni",
+    "Bob": "Mushroom",
+    "Mary": "Cheese"
+}
+name = True
+
+print(name)
